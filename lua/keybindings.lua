efc1d2d47f8d81d98dd8262b72f0441b87ddd6d1
cyejing <jing23501@qq.com>
2022-05-22 22:22:14 +0800

    start

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,12 +1,6 @@
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
--- add your own keymapping
---lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
--- unmap a default keymapping
--- lvim.keys.normal_mode["<C-Up>"] = false
--- edit a default keymapping
--- lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
 lvim.keys.normal_mode = {
     -- basic
