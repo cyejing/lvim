b4db505e714ae9997a55a8a17e93bf44da88d050
chenyejing <chenyejing@didiglobal.com>
2022-05-25 18:58:46 +0800

    1

--- config.lua
+++ config.lua
@@ -16,9 +16,9 @@ require("builtin.fidget")
 
 -- general
 lvim.log.level = "warn"
+lvim.use_icons = true
 lvim.format_on_save = false
 lvim.colorscheme = "onedarker"
 vim.o.background = "dark"
 vim.g.tokyonight_style = "storm"
 -- to disable icons and use a minimalist setup, uncomment the following
-lvim.use_icons = false
--- lua/builtin/nvim-tree.lua
+++ lua/builtin/nvim-tree.lua
@@ -2,5 +2,7 @@ local setup = lvim.builtin.nvimtree.setup
 setup.disable_netrw = true
 setup.git.enable = false
 setup.view.hide_root_folder = true
+setup.view.width = 36
+setup.filters.dotfiles = true
 setup.actions.open_file.resize_window = true
 setup.actions.open_file.quit_on_open = false
