56a848fdb844ddd95e79df56ebfa28ef8d526982
cyejing <jing23501@qq.com>
2022-05-29 14:05:07 +0800

    1

--- lua/builtin.lua
+++ lua/builtin.lua
@@ -16,20 +16,7 @@ lvim.builtin.project.exclude_dirs = { ".local" }
 lvim.builtin.comment.toggler.block = "gcb"
 -- treesitter
 -- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-    "bash",
-    "c",
-    "javascript",
-    "json",
-    "lua",
-    "python",
-    "typescript",
-    "tsx",
-    "css",
-    "rust",
-    "java",
-    "yaml",
-}
+lvim.builtin.treesitter.ensure_installed = { "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx", "css", "rust", "java", "yaml", }
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
