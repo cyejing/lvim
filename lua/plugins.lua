904d44c158934536a2aaf7a6e154870447e54e5e
cyejing <jing23501@qq.com>
2022-06-11 14:51:27 +0800

    1

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -146,16 +146,6 @@ lvim.plugins = {
         end,
     },
     { 'dinhhuy258/vim-local-history' },
-    {
-        'wfxr/minimap.vim',
-        run = "cargo install --locked code-minimap",
-        -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
-        config = function()
-            vim.cmd("let g:minimap_width = 10")
-            vim.cmd("let g:minimap_auto_start = 1")
-            vim.cmd("let g:minimap_auto_start_win_enter = 1")
-        end,
-    },
 
     -- git
     {
