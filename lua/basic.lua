5dbf35f5975575f63bf7b9059063c78307d93f0c
cyejing <chenyejing@didiglobal.com>
2023-04-25 16:47:53 +0800

    chore: neovide

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,4 +1,4 @@
-vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h15"
+vim.opt.guifont = "JetBrainsMono Nerd Font Mono:h17"
 -- utf8
 vim.g.encoding = "UTF-8"
 vim.opt.fileencoding = "utf-8"
@@ -76,3 +76,21 @@ vim.opt.showtabline = 2
 vim.opt.showmode = false
 ---- 配置剪切板
 vim.opt.clipboard = "unnamedplus"
+
+---- neovide config
+if vim.g.neovide then
+  vim.g.neovide_input_use_logo = 1 -- enable use of the logo (cmd) key
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+---- Allow clipboard copy paste in neovim
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
