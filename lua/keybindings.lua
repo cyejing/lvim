876b130da22ece49a25423d49ad3833e17b79da7
cyejing <chenyejing@didiglobal.com>
2023-07-31 10:55:37 +0800

    keymap g

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,7 +4,7 @@ lvim.leader = "space"
 lvim.keys.normal_mode = {
     -- basic
     -- ["s"] = "",
-    ["g"] = "",
+    -- ["g"] = "",
     ["<C-e>"] = "$",
     ["<C-a>"] = "^",
     ["<CR>"] = "<CMD>noh<CR>",
